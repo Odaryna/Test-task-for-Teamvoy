df4bf29fea7788e3274bd23a3f4af7f70db52f8a
Odaryna <odaryna.korolevych@gmail.com>
2018-01-03 01:17:51 +0200

    - Implemented showing user location on the map; - Installed pods for Alamofire.

---
+++ Pods/Target Support Files/Pods-Test-task-for-Teamvoy/Pods-Test-task-for-Teamvoy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Test_task_for_Teamvoy : NSObject
+@end
+@implementation PodsDummy_Pods_Test_task_for_Teamvoy
+@end
--- Test-task-for-Teamvoy/ViewController.swift
+++ Test-task-for-Teamvoy/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import MapKit
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    private var currentLocation: CLLocation?
+    
+    @IBAction func showSunDetailsForCurrentLocation(_ sender: UIButton) {
+        
+        Alamofire.request("https://httpbin.org/get", parameters: ["foo": "bar"])
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .response { response in
+                // response handling code
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+                
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        defer { currentLocation = locations.last }
+        
+        if currentLocation == nil {
+            // Zoom to user location
+            if let userLocation = locations.last {
+                let viewRegion = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 200, 200)
+                mapView.setRegion(viewRegion, animated: false)
+            }
+        }
     }
-
 
 }
 
